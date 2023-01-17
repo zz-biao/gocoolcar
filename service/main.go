fd637dc491dfb1b074f94728300d89df4f098fbc
zz_biao <zz_biao@163.com>
2023-01-17 10:39:15 +0800

    3-3-3-4 grpc-gateway

--- service/main.go
+++ service/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
 	trippb "coolcar/proto/gen/go"
 	trip "coolcar/tripservice"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
 )
 
 func main() {
+	log.SetFlags(log.Lshortfile)
+	go startGRPCGateway()
 	lis, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -18,4 +23,21 @@ func main() {
 
 	trippb.RegisterTripServiceServer(s, &trip.ServiceServer{})
 	log.Fatal(s.Serve(lis))
+}
+
+func startGRPCGateway() {
+	c := context.Background()
+	c, cancel := context.WithCancel(c)
+	defer cancel()
+	mux := runtime.NewServeMux()
+
+	err := trippb.RegisterTripServiceHandlerFromEndpoint(c, mux, "localhost:8081", []grpc.DialOption{grpc.WithInsecure()})
+	if err != nil {
+		log.Fatalf("connot start grpc")
+	}
+
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("connot start grpc")
+	}
 }
