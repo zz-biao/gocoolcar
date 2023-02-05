fe838e27e159365e5ff001a5ca6ed56e23ac85cf
zz_biao <zz_biao@163.com>
2023-02-05 22:27:10 +0800

    前端联调 mongo

--- service/auth/auth/auth.go
+++ service/auth/auth/auth.go
@@ -3,14 +3,15 @@ package auth
 import (
 	"context"
 	authpb "coolcar/auth/api/gen/v1"
+	"coolcar/auth/dao"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"log"
 )
 
 type Service struct {
 	OpenIDResolver OpenIDResolver
+	Mongo          *dao.Mongo
 	Logger         *zap.Logger
 }
 
@@ -20,15 +21,20 @@ type OpenIDResolver interface {
 
 func (s *Service) Login(c context.Context, req *authpb.LoginRequest) (res *authpb.LoginResponse, err error) {
 
-	st, err := s.OpenIDResolver.Resolver(req.Code)
+	openID, err := s.OpenIDResolver.Resolver(req.Code)
 	if err != nil {
 		return nil, status.Errorf(codes.Unavailable, "cannot resolve openid: %v", err)
 	}
-	log.Printf(st)
+
+	accountID, err := s.Mongo.ResolveAccountID(c, openID)
+	if err != nil {
+		s.Logger.Error("cannot resolve account id ", zap.Error(err))
+		return nil, status.Error(codes.Internal, "")
+	}
 
 	s.Logger.Info("received code", zap.String("code", req.Code))
 	return &authpb.LoginResponse{
-		AccessToken: "123123",
+		AccessToken: "token for openID:" + accountID,
 		ExpiresIn:   3600,
 	}, nil
 }
--- service/auth/main.go
+++ service/auth/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	authpb "coolcar/auth/api/gen/v1"
 	"coolcar/auth/auth"
+	"coolcar/auth/dao"
 	"coolcar/auth/wechat"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"log"
@@ -19,12 +23,20 @@ func main() {
 	if err != nil {
 		logger.Fatal("cannot listen", zap.Error(err))
 	}
+
+	c := context.Background()
+	mongoCilent, err := mongo.Connect(c, options.Client().ApplyURI("mongodb://root:123456@localhost:27017/?authSource=admin"))
+	if err != nil {
+		logger.Fatal("cannot connect mongodb", zap.Error(err))
+	}
+
 	s := grpc.NewServer()
 	authpb.RegisterAuthServiceServer(s, &auth.Service{ //设置配置
 		OpenIDResolver: &wechat.Service{
 			AppID:     "sss",
 			AppSecret: "222",
 		},
+		Mongo:  dao.NewMongo(mongoCilent.Database("coolcar")),
 		Logger: logger,
 	})
 	err = s.Serve(lis)
